6a90347a4405fadf80d0e4f2522f06997b7626e4
LucasBalbueno <balbuenolucas04@gmail.com>
2024-03-25 09:13:13 -0300

    Criando validações de email e criptografia de senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,18 +1,36 @@
+// importando a função que criptografa a senha (o hash)
+const { hash } = require('bcryptjs');
+
 // importando a classe de tratamento de erro
 const AppError = require ('../utils/AppError.js');
 
+// importando a conexão com o banco de dados
+const sqliteConnection = require('../database/sqlite')
+
 // criando a classe UsersController para responder as requisições
 class UsersController {
-    create (request, response) {
+    async create (request, response) {
         const { name, email, password } = request.body;
 
-        // se não tiver nome
-        if (!name) {
-            // lançando um erro
-            throw new AppError('Nome é obrigatório!');
+        const database = await sqliteConnection();
+
+        // usando um script SQL para verificar se o usuário já existe
+        // o script retorna tudo de usuário onde o email é igual ao email passado na requisição
+        // Para inserir variaveis no script SQL, usamos o (?) e depois , [variavel1]
+        const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email]);
+
+        if (checkUserExists) {
+            throw new AppError('Este e-mail já está em uso');
         }
 
-        response.json({ name, email, password });
+        // executando a função de hash para criptografar a senha
+        // para hash, passamos a senha e o número de rounds (quantidade de vezes que a senha será criptografada), ou seja, a complexidade da senha
+        const hashedPassword = await hash(password, 8)
+
+        // usando um script SQL para inserir o registro do novo usuário no banco de dados
+        await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword]);
+
+        return response.status(201).json()
     }
 }
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -10,32 +10,12 @@ const UsersController = require('../controllers/UsersController');
 // inicializando o Router do express
 const usersRoutes = Router();
 
-// criando um middleware
-// no middleware temos acesso a requisição, ao objeto resposta e também o seu destino (para onde o middleware deve ir depois)
-function myMiddleware (request, response, next) {
-    // avisando que passou pelo middleware
-    console.log('Você passou pelo Middleware');
-
-    // autenticação para verificar se o usuário é administrador. Se não for, return encerrra a execução do middleware
-    if (!request.body.isAdm) {
-        return response.status(401).json({ error: 'Usuário não autorizado!' });
-    }
-
-    // se passou pela autenticação, irá para o próximo middleware
-    next();
-}
-
-
-
-
-
-
 // criando uma instância da classe UsersController para poder usar ela na rota.
 const usersController = new UsersController();
 
 // criando uma rota para o método POST
 // quando chegarmos na raiz / o usersController irá chamar o método create, mas antes disso, passará pelo middleware (que agora possui acesso a requisição)
-usersRoutes.post('/', myMiddleware, usersController.create);
+usersRoutes.post('/', usersController.create);
 
 // exportando as rotas do usuário
 module.exports = usersRoutes;
