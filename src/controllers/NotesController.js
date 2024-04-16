584468dfe6b3f5b52140a6fd29fc7ef2f09d89d9
LucasBalbueno <balbuenolucas04@gmail.com>
2024-04-16 17:28:41 -0300

    obtendo tags da nota

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -119,8 +119,18 @@ class NotesController {
             .orderBy('title');   
         }
 
+        const userTags = await knex('tags').where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+
         // retornando a resposta da requisição em formato JSON
-        return response.json(notes);
+        return response.json(notesWithTags);
     }
 }
 
