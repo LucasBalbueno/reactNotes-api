ec9732bbc0b1bce9c5001c9b32ad9b18ab943af4
LucasBalbueno <balbuenolucas04@gmail.com>
2024-11-04 08:29:50 -0300

    Criando SessionsController e validação de email e senha

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ const routes = Router();
 const usersRoutes = require('./users.routes');
 const notesRoutes = require('./notes.routes');
 const tagsRoutes = require('./tags.routes');
+const sessionsRouter = require('./sessions.routes');
 
 // toda a vez que alguem acessar a rota /users, o express irá redirecionar para o arquivo users.routes.js que é o grupo de rotas do usuário
 routes.use('/users', usersRoutes);
@@ -18,8 +19,11 @@ routes.use('/users', usersRoutes);
 // toda a vez que alguem acessar a rota /notes, o express irá redirecionar para o arquivo notes.routes.js que é o grupo de rotas de notas
 routes.use('/notes', notesRoutes);
 
-// toda a vez que alguem acessar a rota /notes, o express irá redirecionar para o arquivo tags.routes.js que é o grupo de rotas de notas
+// toda a vez que alguem acessar a rota /tags, o express irá redirecionar para o arquivo tags.routes.js que é o grupo de rotas de notas
 routes.use('/tags', tagsRoutes);
+
+// toda a vez que alguem acessar a rota /sessionsRouter, o express irá redirecionar para o arquivo sessions.routes.js que é o grupo de rotas de notas
+routes.use('/sessions', sessionsRouter);
 
 // exportando as rotas da aplicação, os grupos de rotas
 module.exports = routes;
